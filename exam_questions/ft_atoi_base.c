a6f6fe4ad2da9bf0d070986f57d0f90e9888fdbc
Ayse Goksu Kaya Ozsan <akaya-oz@f1r1s11.codam.nl>
2024-02-02 17:56:58 +0100

    studied for exam

--- exam_questions/ft_atoi_base.c
+++ exam_questions/ft_atoi_base.c
@@ -1,70 +1,70 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-//bence dogru, ayni ciktiyi veriyor ama sistem kabul etmiyor
+// //bence dogru, ayni ciktiyi veriyor ama sistem kabul etmiyor
 
-int	ft_atoi_base(const char *str, int str_base)
-{
-	int		sign;
-	int		i;
-	int		nb;
-	char	*digits;
-	char	*digits2;
-	int	j;
-
-	nb = 0;
-	i = 0;
-	sign = 1;
-	digits = "0123456789abcdef";
-	digits2 = "0123456789ABCDEF";
-	if (str[0] == '-' || str[0] == '+')
-	{
-		if (str[0] == '-')
-			sign = -1;
-		i++;
-	}
-	while (str[i] && j < 17)
-	{
-		j = 0;
-		while (j < 17)
-		{
-			if (str[i] == digits[j] || str[i] == digits2[j])
-				break ;
-			j++;
-		}
-		if (j == 17)
-			i++;
-		else
-		{
-			nb = (nb * str_base) + j;
-			i++;
-		}
-	}
-	return (nb * sign);
-}
-
-// int main()
+// int	ft_atoi_base(const char *str, int str_base)
 // {
-// 	printf("%d\n", ft_atoi_base("Ceci permet de decouvrir le fonctionnement de ton ft_atoi_base", 16));
-// 	return (0);
+// 	int		sign;
+// 	int		i;
+// 	int		nb;
+// 	char	*digits;
+// 	char	*digits2;
+// 	int	j;
+
+// 	nb = 0;
+// 	i = 0;
+// 	sign = 1;
+// 	digits = "0123456789abcdef";
+// 	digits2 = "0123456789ABCDEF";
+// 	if (str[0] == '-' || str[0] == '+')
+// 	{
+// 		if (str[0] == '-')
+// 			sign = -1;
+// 		i++;
+// 	}
+// 	while (str[i] && j < 17)
+// 	{
+// 		j = 0;
+// 		while (j < 17)
+// 		{
+// 			if (str[i] == digits[j] || str[i] == digits2[j])
+// 				break ;
+// 			j++;
+// 		}
+// 		if (j == 17)
+// 			i++;
+// 		else
+// 		{
+// 			nb = (nb * str_base) + j;
+// 			i++;
+// 		}
+// 	}
+// 	return (nb * sign);
 // }
 
-// Assignment name  : ft_atoi_base
-// Expected files   : ft_atoi_base.c
-// Allowed functions: None
-// --------------------------------------------------------------------------------
+// // int main()
+// // {
+// // 	printf("%d\n", ft_atoi_base("Ceci permet de decouvrir le fonctionnement de ton ft_atoi_base", 16));
+// // 	return (0);
+// // }
 
-// Write a function that converts the string argument str (base N <= 16)
-// to an integer (base 10) and returns it.
+// // Assignment name  : ft_atoi_base
+// // Expected files   : ft_atoi_base.c
+// // Allowed functions: None
+// // --------------------------------------------------------------------------------
 
-// The characters recognized in the input are: 0123456789abcdef
-// Those are, of course, to be trimmed according to the requested base. For
-// example, base 4 recognizes "0123" and base 16 recognizes "0123456789abcdef".
+// // Write a function that converts the string argument str (base N <= 16)
+// // to an integer (base 10) and returns it.
 
-// Uppercase letters must also be recognized: "12fdb3" is the same as "12FDB3".
+// // The characters recognized in the input are: 0123456789abcdef
+// // Those are, of course, to be trimmed according to the requested base. For
+// // example, base 4 recognizes "0123" and base 16 recognizes "0123456789abcdef".
 
-// Minus signs ('-') are interpreted only if they are the first character of the
-// string.
+// // Uppercase letters must also be recognized: "12fdb3" is the same as "12FDB3".
 
-// Your function must be declared as follows:
+// // Minus signs ('-') are interpreted only if they are the first character of the
+// // string.
 
-// int	ft_atoi_base(const char *str, int str_base);
+// // Your function must be declared as follows:
+
+// // int	ft_atoi_base(const char *str, int str_base);
