823d33c573fb1c92f1b548542e4ed7600a1024e5
Ayse Goksu Kaya Ozsan <akaya-oz@f1r6s19.codam.nl>
2024-02-27 14:00:14 +0100

    started pipex

---
+++ pipex/fork.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("%d\n",fork());
+	printf("%d\n",fork());
+	return (0);
+}
+
+// cc fork.c && ./a.out 
+// 122941 // A says (parent) I have B as child with that PID
+// 122942 // A says (parent) I have C as child with that PID [or B (new parent) says I have D as child with that PID, we dont know the order]
+// 0 // C says Im a child
+// 122943 // B (new parent) says I have D as child with that PID
+// 0 // D says Im a child
+// 0 // B says I was a child 
+
+// first fork:
+//  A --> B
+
+// second fork:
+//  A --> B --> D
+//  |
+//  A --> C
