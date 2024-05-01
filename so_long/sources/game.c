47e430fb072f7f8659428bfc5702aac50182b5d7
Ayse Goksu Kaya Ozsan <akaya-oz@student.codam.nl>
2024-05-01 11:14:32 +0200

    can see a colorful map

--- so_long/sources/game.c
+++ so_long/sources/game.c
@@ -6,7 +6,7 @@
 /*   By: akaya-oz <akaya-oz@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/29 20:35:53 by akaya-oz      #+#    #+#                 */
-/*   Updated: 2024/04/29 23:53:13 by akaya-oz      ########   odam.nl         */
+/*   Updated: 2024/05/01 11:13:18 by akaya-oz      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,27 @@ void	sl_free_textures(t_game *game)
 		mlx_delete_texture(game->gameover_tx);
 }
 
-void	sl_free_images(t_game *game)
+void	sl_free_images(t_game *game, t_map *my_map)
 {
 	if (game->char_img)
-		mlx_delete_image(game->char_img);
+		mlx_delete_image(my_map->mlx, game->char_img);
 	if (game->wall_img)
-		mlx_delete_image(game->wall_img);
+		mlx_delete_image(my_map->mlx, game->wall_img);
 	if (game->col_img)
-		mlx_delete_image(game->col_img);
+		mlx_delete_image(my_map->mlx, game->col_img);
 	if (game->end_img)
-		mlx_delete_image(game->end_img);
+		mlx_delete_image(my_map->mlx, game->end_img);
 	if (game->backgr_img)
-		mlx_delete_image(game->backgr_img);
+		mlx_delete_image(my_map->mlx, game->backgr_img);
 	if (game->fire_img)
-		mlx_delete_image(game->fire_img);
+		mlx_delete_image(my_map->mlx, game->fire_img);
 	if (game->gameover_img)
-		mlx_delete_image(game->gameover_img);
+		mlx_delete_image(my_map->mlx, game->gameover_img);
 	if (game->steps_img)
-		mlx_delete_image(game->steps_img);
+		mlx_delete_image(my_map->mlx, game->steps_img);
 }
 
-bool	sl_image_init(game)
+bool	sl_image_init(t_game *game, t_map *my_map)
 {
 	game->char_tx = mlx_load_png("textures/char.png");
 	game->wall_tx = mlx_load_png("textures/wall.png");
@@ -59,7 +59,7 @@ bool	sl_image_init(game)
 	game->backgr_tx = mlx_load_png("textures/backgr.png");
 	game->fire_tx = mlx_load_png("textures/fire.png");
 	game->gameover_tx = mlx_load_png("textures/gameover.png");
-	game->steps_img = mlx_new_image(mlx, 64, 64);
+	game->steps_img = mlx_new_image(my_map->mlx, 64, 64);
 	if (game->char_tx == NULL || game->wall_tx == NULL || game->col_tx == NULL
 		|| game->end_tx == NULL || game->backgr_tx == NULL
 		|| game->fire_tx == NULL || game->gameover_tx == NULL
@@ -126,38 +126,38 @@ bool	sl_place_images(t_map *my_map, t_game *game)
 void	sl_clean_window(t_game *game, t_map *my_map)
 {
 	sl_free_textures(game);
-	sl_free_images(game);
+	sl_free_images(game, my_map);
 	mlx_terminate(my_map->mlx);
 }
 
-void	ft_hook(void *pointer)
-{
-	mlx_t	*mlx;
+// void	ft_hook(void *pointer)
+// {
+// 	mlx_t	*mlx;
 
-	mlx = (mlx_t *)pointer;
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
-}
+// 	mlx = (mlx_t *)pointer;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+// 		mlx_close_window(mlx);
+// 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+// 		image->instances[0].y -= 5;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+// 		image->instances[0].y += 5;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+// 		image->instances[0].x -= 5;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+// 		image->instances[0].x += 5;
+// }
 
 bool	sl_create_window(t_game *game, t_map *my_map)
 {
 	mlx_t	*mlx;
 
-	my_map->game = game;
-	my_map->mlx = mlx;
-	mlx = mlx_init(map->length_x * 64, map->length_y * 64, "So_long", false);
+	mlx = mlx_init(my_map->length_x * 32, my_map->length_y * 32, "So_long",
+			false);
 	if (mlx == NULL)
 		return (false);
+	my_map->mlx = mlx;
 	my_map->game.steps = 0;
-	if (!sl_image_init(game))
+	if (!sl_image_init(game, my_map))
 		return (sl_clean_window(game, my_map), false);
 	if (!sl_text_to_img(my_map, game))
 		return (sl_clean_window(game, my_map), false);
@@ -166,7 +166,7 @@ bool	sl_create_window(t_game *game, t_map *my_map)
 	game->char_y = my_map->player_y;
 	game->char_x = my_map->player_x;
 	my_map->game.dead = 0;
-	mlx_loop_hook(my_map->mlx, ft_hook, my_map->mlx);
+	// mlx_loop_hook(my_map->mlx, ft_hook, my_map->mlx);
 	mlx_loop(my_map->mlx);
 	sl_clean_window(game, my_map);
 	return (true);
