329dca0979b8a0bde98a986893bd95d5a4aeceb5
Ayse Goksu Kaya Ozsan <akaya-oz@student.codam.nl>
2024-05-07 01:27:56 +0200

    studied philo

--- philo1/includes/philo.h
+++ philo1/includes/philo.h
@@ -4,43 +4,47 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <pthread.h>
 
 typedef struct s_chair
 {
-	int			index;
+	int					index;
 	// int			philo_index;
-	int			left_fork_index;
-	int			right_fork_index;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			number_of_times_each_philosopher_must_eat;
-}				t_chair;
-
-typedef struct s_fork
-{
-	int			index;
-}				t_fork;
+	int					left_fork_index;
+	int					right_fork_index;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					number_of_times_each_philosopher_must_eat;
+}						t_chair;
 
 typedef struct s_seating
 {
 	t_chair				*chair;
 	struct s_seating	*next;
-}				t_seating;
+}						t_seating;
 
 typedef struct s_table
 {
-	int			number_of_philosophers;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			number_of_times_each_philosopher_must_eat;
-	t_seating	*seating;
-}				t_table;
+	int					number_of_philosophers;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					number_of_times_each_philosopher_must_eat;
+	t_seating			*seating;
+}						t_table;
+
+// table.c
+
+void	free_table_and_others(t_table *table, int number_of_philosophers);
+void	make_seating(t_table *table);
+t_table	*table_init(char *number_of_philosophers, char *time_to_die,
+		char *time_to_eat, char *time_to_sleep,
+		char *number_of_times_each_philosopher_must_eat);
 
 // utils.c
 
-int				ft_atoi(const char *nptr);
-char			*ft_itoa(int n);
+int						ft_atoi(const char *nptr);
+char					*ft_itoa(int n);
 
 #endif
--- philo1/sources/main.c
+++ philo1/sources/main.c
@@ -6,117 +6,17 @@
 /*   By: akaya-oz <akaya-oz@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/03 12:19:43 by akaya-oz      #+#    #+#                 */
-/*   Updated: 2024/05/03 15:03:04 by akaya-oz      ########   odam.nl         */
+/*   Updated: 2024/05/03 15:09:40 by akaya-oz      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-
-void	free_table_and_others(t_table *table, int number_of_philosophers)
-{
-	t_seating	*seating;
-	t_seating	*next;
-
-	seating = table->seating;
-	while (number_of_philosophers--)
-	{
-		next = seating->next;
-		free(seating->chair);
-		free(seating);
-		seating = next;
-	}
-	free(table);
-}
-
-
-
-void	make_seating(t_table *table)
-{
-	int			i;
-	t_seating	*current;
-	t_seating	*head;
-
-	i = 0;
-	head = malloc(sizeof(t_seating));
-	// printf("t_seating malloc\n");
-	if (!head)
-	{
-		printf("Error: Seating malloc failed\n");
-		exit(3);
-	}
-	current = head;
-	while (i < table->number_of_philosophers)
-	{
-		current->chair = malloc(sizeof(t_chair));
-		// printf("t_chair malloc\n");
-		if (!current->chair)
-		{
-			printf("Error: Chair malloc failed\n");
-			exit(4);
-		}
-		current->chair->index = i;
-		current->chair->left_fork_index = i;
-		current->chair->time_to_die = table->time_to_die;
-		current->chair->time_to_eat = table->time_to_eat;
-		current->chair->time_to_sleep = table->time_to_sleep;
-		current->chair->number_of_times_each_philosopher_must_eat = table->number_of_times_each_philosopher_must_eat;
-		if (i == table->number_of_philosophers - 1)
-		{
-			current->chair->right_fork_index = 0;
-			current->next = head;
-		}
-		else
-		{
-			current->chair->right_fork_index = i + 1;
-			current->next = malloc(sizeof(t_seating));
-			// printf("t_seating malloc\n");
-			if (!current->next)
-			{
-				printf("Error: New chair malloc failed\n");
-				exit(5);
-			}
-		}
-		i++;
-		current = current->next;
-	}
-	table->seating = head;
-}
 
 void	philo(t_table *table)
 {
 	make_seating(table);
 	// printf("seating made\n");
 
-}
-
-t_table	*table_init(char *number_of_philosophers, char *time_to_die,
-		char *time_to_eat, char *time_to_sleep,
-		char *number_of_times_each_philosopher_must_eat)
-{
-	t_table	*table;
-
-	table = malloc(sizeof(t_table));
-	// printf("t_table malloc\n");
-	if (!table)
-	{
-		printf("Error: Table malloc failed\n");
-		exit(2);
-	}
-	table->number_of_philosophers = ft_atoi(number_of_philosophers);
-	table->time_to_die = ft_atoi(time_to_die);
-	table->time_to_eat = ft_atoi(time_to_eat);
-	table->time_to_sleep = ft_atoi(time_to_sleep);
-	if (number_of_times_each_philosopher_must_eat)
-		table->number_of_times_each_philosopher_must_eat = ft_atoi(number_of_times_each_philosopher_must_eat);
-	else
-		table->number_of_times_each_philosopher_must_eat = -1;
-	// printf("number_of_philosophers: %d\n", table->number_of_philosophers);
-	// printf("time_to_die: %d\n", table->time_to_die);
-	// printf("time_to_eat: %d\n", table->time_to_eat);
-	// printf("time_to_sleep: %d\n", table->time_to_sleep);
-	// printf("number_of_times_each_philosopher_must_eat: %d\n",
-	// 	table->number_of_times_each_philosopher_must_eat);
-	return (table);
 }
 
 int	main(int argc, char **argv)
