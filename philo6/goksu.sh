589a799e2fda6587efccce5b4ecb9d4c50deb3e0
Ayse Goksu Kaya Ozsan <akaya-oz@f1r2s5.codam.nl>
2024-07-11 17:07:21 +0200

    philo passed

--- philo6/goksu.sh
+++
@@ -1,8 +0,0 @@
-
-
-
-
-> normal.txt && make && ./philo 5 800 200 200 1 | cat >> normal.txt
-#sleep 60 && exec <&-
-> bonus.txt && make bonus && ./philo_bonus 5 800 200 200 7 | cat >> bonus.txt
-#sleep 60 && exec <&-
